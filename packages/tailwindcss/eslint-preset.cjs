eb872ef9f73fd7837ae3d8a52042daa2b0a076e6
Leonard Nguyen <minhktmd@gmail.com>
2025-07-25 14:25:21 +0700

    update tailwind config

---
+++ packages/tailwindcss/eslint-preset.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'plugin:tailwindcss/recommended',
+  ],
+  plugins: ['tailwindcss'],
+  rules: {
+    'tailwindcss/no-custom-classname': 'warn',
+    'tailwindcss/no-contradicting-classname': 'error',
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/no-arbitrary-value': 'off',
+  },
+  settings: {
+    tailwindcss: {
+      callees: ['classnames', 'clsx', 'cn'],
+      config: 'tailwind.config.ts',
+    },
+  },
+};
